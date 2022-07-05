874b8be9f863ae7a99a2ae64b45ef5a33c467f7c
Abhishek2k3 <kumar.abhi2k3@gmail.com>
2022-07-05 23:38:06 +0530

    version 1.0

---
+++ script.js
@@ -0,0 +1,19 @@
+const panels =document.querySelectorAll('.panel')
+
+panels.forEach(panel=> {
+   
+    panel.addEventListener('click', () =>{
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+    
+
+})
+
+function removeActiveClasses(){
+    panels.forEach(panels =>{
+       
+        panels.classList.remove('active')
+        
+    })
+}
